29b410f05d5652917940e9bc22b7533eead9b2c3
zulfikar4568 <isnaen70@gmail.com>
2022-10-01 15:42:24 +0700

    feat: versioning api

--- dockerfiles/kong/setup-notification.sh
+++ dockerfiles/kong/setup-notification.sh
@@ -3,7 +3,15 @@
 # Service
 curl -X POST http://localhost:8001/services \
   -H "Content-Type: application/json" \
-  -d '{"name": "notification-service", "host": "host.docker.internal", "port": 3004, "path": "/api/v1/notification"}'
+  -d '{"name": "notification-service-v1", "host": "host.docker.internal", "port": 3004, "path": "/api/v1/notification"}'
+
+curl -X POST http://localhost:8001/services \
+  -H "Content-Type: application/json" \
+  -d '{"name": "notification-service-v2", "host": "host.docker.internal", "port": 3004, "path": "/api/v2/notification"}'
+
+curl -X POST http://localhost:8001/services \
+  -H "Content-Type: application/json" \
+  -d '{"name": "notification-public", "host": "host.docker.internal", "port": 3004, "path": "/api/notification/public"}'
 
 curl -X POST http://localhost:8001/services \
   -H "Content-Type: application/json" \
@@ -11,9 +19,17 @@ curl -X POST http://localhost:8001/services \
 
 
 # Route
-curl -X POST http://localhost:8001/services/notification-service/routes/ \
+curl -X POST http://localhost:8001/services/notification-service-v1/routes/ \
   -H "Content-Type: application/json" \
-  -d '{"name": "notification-route", "paths": [ "/api/v1/notification" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
+  -d '{"name": "notification-route-v1", "paths": [ "/api/v1/notification" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
+
+curl -X POST http://localhost:8001/services/notification-service-v2/routes/ \
+  -H "Content-Type: application/json" \
+  -d '{"name": "notification-route-v2", "paths": [ "/api/v2/notification" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
+
+curl -X POST http://localhost:8001/services/notification-public/routes/ \
+  -H "Content-Type: application/json" \
+  -d '{"name": "notification-route-public", "paths": [ "/api/notification/public" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
 
 curl -X POST http://localhost:8001/services/notification-openapi/routes/ \
   -H "Content-Type: application/json" \
--- dockerfiles/kong/setup-things.sh
+++ dockerfiles/kong/setup-things.sh
@@ -3,7 +3,15 @@
 # Service
 curl -X POST http://localhost:8001/services \
   -H "Content-Type: application/json" \
-  -d '{"name": "things-service", "host": "host.docker.internal", "port": 3003, "path": "/api/v1/things"}'
+  -d '{"name": "things-service-v1", "host": "host.docker.internal", "port": 3003, "path": "/api/v1/things"}'
+
+curl -X POST http://localhost:8001/services \
+  -H "Content-Type: application/json" \
+  -d '{"name": "things-service-v2", "host": "host.docker.internal", "port": 3003, "path": "/api/v2/things"}'
+
+curl -X POST http://localhost:8001/services \
+  -H "Content-Type: application/json" \
+  -d '{"name": "things-public", "host": "host.docker.internal", "port": 3003, "path": "/api/things/public"}'
 
 curl -X POST http://localhost:8001/services \
   -H "Content-Type: application/json" \
@@ -11,9 +19,17 @@ curl -X POST http://localhost:8001/services \
 
 
 # Route
-curl -X POST http://localhost:8001/services/things-service/routes/ \
+curl -X POST http://localhost:8001/services/things-service-v1/routes/ \
   -H "Content-Type: application/json" \
-  -d '{"name": "things-route", "paths": [ "/api/v1/things" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
+  -d '{"name": "things-route-v1", "paths": [ "/api/v1/things" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
+
+curl -X POST http://localhost:8001/services/things-service-v2/routes/ \
+  -H "Content-Type: application/json" \
+  -d '{"name": "things-route-v2", "paths": [ "/api/v2/things" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
+
+curl -X POST http://localhost:8001/services/things-public/routes/ \
+  -H "Content-Type: application/json" \
+  -d '{"name": "things-route-public", "paths": [ "/api/things/public" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
 
 curl -X POST http://localhost:8001/services/things-openapi/routes/ \
   -H "Content-Type: application/json" \
