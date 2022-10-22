7391285690b6a9e6c240c38ac99eb536c65617b0
zulfikar4568 <isnaen70@gmail.com>
2022-10-22 18:59:22 +0700

    feat: Audit kong development

---
+++ dockerfiles/kong/setup-audit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Service
+curl -X POST http://localhost:8001/services \
+  -H "Content-Type: application/json" \
+  -d '{"name": "audit-service-v1", "host": "host.docker.internal", "port": 3003, "path": "/api/v1/audit"}'
+
+curl -X POST http://localhost:8001/services \
+  -H "Content-Type: application/json" \
+  -d '{"name": "audit-service-v2", "host": "host.docker.internal", "port": 3003, "path": "/api/v2/audit"}'
+
+curl -X POST http://localhost:8001/services \
+  -H "Content-Type: application/json" \
+  -d '{"name": "audit-public", "host": "host.docker.internal", "port": 3003, "path": "/api/audit/public"}'
+
+curl -X POST http://localhost:8001/services \
+  -H "Content-Type: application/json" \
+  -d '{"name": "audit-openapi", "host": "host.docker.internal", "port": 3003, "path": "/api/audit"}'
+
+
+# Route
+curl -X POST http://localhost:8001/services/audit-service-v1/routes/ \
+  -H "Content-Type: application/json" \
+  -d '{"name": "audit-route-v1", "paths": [ "/api/v1/audit" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
+
+curl -X POST http://localhost:8001/services/audit-service-v2/routes/ \
+  -H "Content-Type: application/json" \
+  -d '{"name": "audit-route-v2", "paths": [ "/api/v2/audit" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
+
+curl -X POST http://localhost:8001/services/audit-public/routes/ \
+  -H "Content-Type: application/json" \
+  -d '{"name": "audit-route-public", "paths": [ "/api/audit/public" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
+
+curl -X POST http://localhost:8001/services/audit-openapi/routes/ \
+  -H "Content-Type: application/json" \
+  -d '{"name": "audit-route-openapi", "paths": [ "/docs/v1/audit", "/docs/audit" ], "protocols": [ "http", "https" ], "methods": [ "GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" ]}'
