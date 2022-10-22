2a8c8765cf28df14041c970561b51ac26262e772
zulfikar4568 <isnaen70@gmail.com>
2022-10-22 19:07:34 +0700

    fix: port kong audit

--- dockerfiles/kong/setup-audit.sh
+++ dockerfiles/kong/setup-audit.sh
@@ -3,19 +3,19 @@
 # Service
 curl -X POST http://localhost:8001/services \
   -H "Content-Type: application/json" \
-  -d '{"name": "audit-service-v1", "host": "host.docker.internal", "port": 3003, "path": "/api/v1/audit"}'
+  -d '{"name": "audit-service-v1", "host": "host.docker.internal", "port": 3005, "path": "/api/v1/audit"}'
 
 curl -X POST http://localhost:8001/services \
   -H "Content-Type: application/json" \
-  -d '{"name": "audit-service-v2", "host": "host.docker.internal", "port": 3003, "path": "/api/v2/audit"}'
+  -d '{"name": "audit-service-v2", "host": "host.docker.internal", "port": 3005, "path": "/api/v2/audit"}'
 
 curl -X POST http://localhost:8001/services \
   -H "Content-Type: application/json" \
-  -d '{"name": "audit-public", "host": "host.docker.internal", "port": 3003, "path": "/api/audit/public"}'
+  -d '{"name": "audit-public", "host": "host.docker.internal", "port": 3005, "path": "/api/audit/public"}'
 
 curl -X POST http://localhost:8001/services \
   -H "Content-Type: application/json" \
-  -d '{"name": "audit-openapi", "host": "host.docker.internal", "port": 3003, "path": "/api/audit"}'
+  -d '{"name": "audit-openapi", "host": "host.docker.internal", "port": 3005, "path": "/api/audit"}'
 
 
 # Route
