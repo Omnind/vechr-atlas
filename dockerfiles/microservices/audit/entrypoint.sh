eebe0010834be7ada217e1e60d52e570d9dc9828
zulfikar4568 <isnaen70@gmail.com>
2022-10-22 18:53:59 +0700

    feat: Audit Trail

---
+++ dockerfiles/microservices/audit/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+PACKAGE_FILE=/app/package.json
+YARN_LOCK_FILE=/app/yarn.lock
+if [ -f "$PACKAGE_FILE" ] && [ -f "$YARN_LOCK_FILE" ]; then
+  echo "yarn install..."
+  yarn install \
+    && yarn prisma:sync \
+    && yarn db:migrate
+fi
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
