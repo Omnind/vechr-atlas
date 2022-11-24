c080e66b8b56427a2ff406a2d0e546cfb055d2b8
zulfikar4568 <isnaen70@gmail.com>
2022-11-25 01:38:15 +0700

    feat: kubernetes full service

--- kubernetes/deploy-init.sh
+++ kubernetes/deploy-init.sh
@@ -3,6 +3,8 @@
 # Setup posgres
 helm install things-db postgres --values ./postgres/things-db.values.yaml \
   && helm install notification-db postgres --values ./postgres/notification-db.values.yaml \
+  && helm install auth-db postgres --values ./postgres/auth-db.values.yaml \
+  && helm install audit-db postgres --values ./postgres/audit-db.values.yaml \
   && helm install influxdb influx --values ./influx/values.yaml \
   && kubectl apply -f ./mail/gmail-external-service.yaml \
   && helm repo add nats https://nats-io.github.io/k8s/helm/charts/ \
@@ -11,8 +13,13 @@ helm install things-db postgres --values ./postgres/things-db.values.yaml \
   && helm install things-service service --values ./service/things-service.values.yaml \
   && helm install notification-service service --values ./service/notification-service.values.yaml \
   && helm install db-logger-service service --values ./service/db-logger-service.values.yaml \
+  && helm install auth-service service --values ./service/auth-service.values.yaml \
+  && helm install audit-service service --values ./service/audit-service.values.yaml \
   && helm install web-app fe --values ./fe/values.yaml \
+  && kubectl create namespace kong \
   && helm install kong -n kong kong/kong -f kong/values.yaml \
   && kubectl apply -f ./kong/things-ingress.yaml \
   && kubectl apply -f ./kong/notification-ingress.yaml \
+  && kubectl apply -f ./kong/auth-ingress.yaml \
+  && kubectl apply -f ./kong/audit-ingress.yaml \
   && kubectl apply -f ./kong/fe-ingress.yaml
